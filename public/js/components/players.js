242caa1cc8b8b512bb8482979ce9a64519e82ca0
Dale Shoulders <dshoulders@outlook.com>
2021-09-04 23:05:16 +0100

    Add score graphic

--- public/js/components/players.js
+++ public/js/components/players.js
@@ -1,16 +1,55 @@
+import { useState } from '../hooks/lib.js'
+import { useRead } from '../hooks/providers/firebase.js'
 import { html } from '../utils.js'
 
 export function players({ registeredPlayers, currentPlayerId }) {
+    const [board, setBoard] = useState({})
+
+    useRead('/board', (snapshot) => {
+        setBoard(snapshot.val())
+    })
+
+    const scores = registeredPlayers.map(
+        (_, index) =>
+            Object.values(board).filter((playerIndex) => playerIndex === index)
+                .length
+    )
+
     return html`
         <ul class="players">
             ${registeredPlayers.map(
-                (player) =>
+                (player, index) =>
                     html`<li
                         class=${`${
                             currentPlayerId === player.uid ? 'active' : ''
                         }`}
                     >
-                        ${player.name}
+                        <img src=${player.avatar} />
+                        <span class="player-name"
+                            >${player.name.split(' ')[0]}</span
+                        >
+                        <div class="player-score">
+                            <span class="player-score-number"
+                                >${scores[index]}</span
+                            >
+                            <svg class="player-score-graphic" height="15">
+                                ${Array(scores[index])
+                                    .fill()
+                                    .map(
+                                        (_, scoreIndex) => html`
+                                            <rect
+                                                x=${scoreIndex * 10}
+                                                y=${0}
+                                                height=${15}
+                                                width=${4}
+                                                fill="white"
+                                                stroke="black"
+                                                stroke-width="1"
+                                            />
+                                        `
+                                    )}
+                            </svg>
+                        </div>
                     </li>`
             )}
         </ul>
